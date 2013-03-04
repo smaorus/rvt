b627428ee44fe12216e631271ff800d8b9c69712
oferst24 <oferst24@gmail.com>
2013-03-04 14:56:43 +0000

    a

--- cbmc_trunk/cbmc_trunk_windows/minisat-2.2.0/utils/depend.mk
+++
@@ -1,7 +0,0 @@
-/home/edima/minisat/utils/Options.o /home/edima/minisat/utils/Options.or /home/edima/minisat/utils/Options.od /home/edima/minisat/utils/Options.op: /home/edima/minisat/utils/Options.cc \
-  /home/edima/minisat/mtl/Sort.h /home/edima/minisat/mtl/Vec.h \
-  /home/edima/minisat/mtl/IntTypes.h /home/edima/minisat/mtl/XAlloc.h \
-  /home/edima/minisat/utils/Options.h \
-  /home/edima/minisat/utils/ParseUtils.h
-/home/edima/minisat/utils/System.o /home/edima/minisat/utils/System.or /home/edima/minisat/utils/System.od /home/edima/minisat/utils/System.op: /home/edima/minisat/utils/System.cc \
-  /home/edima/minisat/utils/System.h /home/edima/minisat/mtl/IntTypes.h
